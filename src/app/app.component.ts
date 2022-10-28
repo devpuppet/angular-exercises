471b8c3cac1eef79a90626de4dfd8bbced8701a9
Kamil <qkiz288@gmail.com>
2022-10-28 09:04:56 +0200

    Angular bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularApp';
+  protected readonly firstName = 'Kamil';
+  protected readonly lastName = 'Kukiełka';
+  title = () => `${this.firstName} ${this.lastName}`;
+  color = "green";
+  protected readonly items = [new Item('Is'), new Item('Angular'), new Item('So'), new Item('Cool?')];
+  protected specificItem: Item | undefined;
+  protected anotherSpecificItem: Item | undefined = new Item('Wow!');
+  isDisabled = true;
+  catUrl = "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=620&quality=45&dpr=2&s=none";
+  catWidth = 200;
+  catHeight = 150;
+  closeLabel = "Close!";
+  inputValue = '';
+  name2 = 'Basic name';
+  name3 = 'More basic name';
+  @Input() count = 0;
+
+  public closeMe() {
+    console.log('Close button clicked!');
+  }
+
+  public closeMeAgain() {
+    console.log('Second method triggered!');
+  }
+
+  public doSomething(event: Event) {
+    this.inputValue = (event.target as HTMLInputElement).value;
+    console.log(`Event payload: ${(event.target as HTMLInputElement).value}`);
+  }
+
+  public doSomethingElse(element: HTMLInputElement) {
+    this.inputValue = element.value;
+    console.log(`Template reference variable: ${element.value}`);
+  }
+
+  clearName() {
+    this.name2= '';
+  }
+}
+
+class Item {
+  constructor(public readonly name: string) {}
 }
