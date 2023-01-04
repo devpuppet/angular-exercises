f74f0dd98096423b138465a979f7b6825cc9400d
Kamil <qkiz288@gmail.com>
2023-01-04 13:57:44 +0100

    @Input on child component setter

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,13 +1,35 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
   styleUrls: ['./counter.component.css']
 })
-export class CounterComponent {
-  @Input() count: number = 0;
+export class CounterComponent implements OnChanges {
+
+  _count: number = 0;
+
   @Output() countChange: EventEmitter<number> = new EventEmitter<number>();
+
+  ngOnChanges(changes: SimpleChanges): void {
+    for (const property in changes) {
+      if (property === 'count') {
+        console.log('Previous:', changes[property].previousValue);
+        console.log('Current:', changes[property].currentValue);
+        console.log('firstChange:', changes[property].firstChange);
+      }
+    }
+  }
+
+  @Input()
+  set count(count: number) {
+    this._count = count;
+    console.log('From @Input setter: ', count);
+  }
+
+  get count(): number {
+    return this._count;
+  }
 
   increment() {
     this.count++;
