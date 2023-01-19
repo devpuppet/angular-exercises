8e792e7123f6354bb0cdec0b465ce0e288345397
Kamil <qkiz288@gmail.com>
2023-01-19 14:52:25 +0100

    Reactive form validators

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -7,21 +7,46 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./reactive-form.component.css']
 })
 export class ReactiveFormComponent implements OnInit {
-  contactForm = new FormGroup({
-    firstname: new FormControl('', [Validators.required, Validators.minLength(2)]),
-    lastname: new FormControl('Kuk'),
-    email: new FormControl({ value: 'mail@example.com', disabled: true }),
-    gender: new FormControl(),
-    isMarried: new FormControl(),
-    address:new FormGroup({
-      city: new FormControl(),
-      street: new FormControl(),
-      pincode:new FormControl(),
-      country: new FormControl(),
-    })
-  });
+  // this is one way to create a form
+  // contactForm = new FormGroup({
+  //   firstname: new FormControl('', [Validators.required, Validators.minLength(2)]),
+  //   lastname: new FormControl('Kuk'),
+  //   email: new FormControl({ value: 'mail@example.com', disabled: true }),
+  //   gender: new FormControl(),
+  //   isMarried: new FormControl(),
+  //   address:new FormGroup({
+  //     city: new FormControl(),
+  //     street: new FormControl(),
+  //     pincode:new FormControl(),
+  //     country: new FormControl(),
+  //   })
+  // });
+  contactForm: FormGroup;
 
-  constructor() { }
+  //another way to create a form is via FormBuilder
+  constructor(private formBuilder: FormBuilder) {
+    this.contactForm = this.formBuilder.group({
+      firstname: ['', [Validators.required, Validators.minLength(5)]],
+      lastname: ['', [Validators.required, Validators.maxLength(20), Validators.pattern("^[a-zA-Z]+$")]],
+      email: ['', [Validators.required, Validators.email]],
+      gender: ['', [Validators.required]],
+      isMarried: ['', [Validators.required]],
+      country: ['', [Validators.required]],
+      address: this.formBuilder.group({
+        city: ['', [Validators.required]],
+        street: ['', [Validators.required]],
+        pincode: ['', [Validators.required]]
+      })
+    })
+  }
+
+  get firstname() {
+    return this.contactForm.get('firstname');
+  }
+
+  get lastname() {
+    return this.contactForm.get('lastname');
+  }
 
   ngOnInit(): void {
   }
