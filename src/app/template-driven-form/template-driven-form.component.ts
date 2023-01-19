82705b2a342e59f92d830d5a7c4f7a3f3ad71441
Kamil <qkiz288@gmail.com>
2023-01-19 12:25:17 +0100

    Set values in template driven form

--- src/app/template-driven-form/template-driven-form.component.ts
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-driven-form',
@@ -6,23 +7,82 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./template-driven-form.component.css']
 })
 export class TemplateDrivenFormComponent implements OnInit {
+  contact!: Contact;
+
   countryList: Country[] = [
     new Country("1", "Poland"),
     new Country("2", "USA"),
     new Country("3", "Serbia")
   ];
 
+  @ViewChild('contactForm') contactForm!: NgForm;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.setDefaults();
   }
 
   onSubmit(contactForm: any) {
     console.log('Contact Form: ', contactForm.value);
   }
 
+  setDefaults() {
+    this.contact = {
+      firstname: "Kamil",
+      lastname: "Kuk",
+      email: "mail@gmail.com",
+      gender: "male",
+      isMarried: true,
+      country: "1",
+      address: { city: "Cracow", street: "Kolorowa", pincode: "12-345" }
+    };
+
+    setTimeout(() => {
+      this.contactForm.controls['country'].setValue('1');
+    });
+  }
+
+  reset(contactForm: NgForm) {
+    contactForm.resetForm();
+  }
+
+  patch() {
+    const obj = {
+      lastname: 'Kuk',
+      email: 'mail@mail.com'
+    }
+
+    this.contactForm.control.patchValue(obj);
+  }
+
+  patchNested() {
+    const obj = {
+      city: 'Warsaw',
+      street: 'Long',
+      pincode: '99-999'
+    }
+
+    const address = this.contactForm.controls['address'] as FormGroup;
+    address.patchValue(obj);
+  }
+
 }
 
 export class Country {
   constructor(public id: string, public name: string) {}
+}
+
+export interface Contact {
+  firstname: string;
+  lastname: string;
+  email: string;
+  gender: string;
+  isMarried: boolean;
+  country: string;
+  address: {
+    city:string;
+    street:string;
+    pincode:string;
+  }
 }
